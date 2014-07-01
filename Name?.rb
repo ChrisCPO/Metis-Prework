71acd0c8961b8c459d774fd53e86a6c286d1d2bb
ChrisCPO <ChrisCPO@gmail.com>
2014-07-01 11:48:49 -0400

    easy upgrade name later

---
+++ Name?.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+puts "what is your first name"
+fname= gets.chomp
+
+puts " what is your middle name,"
+mname= gets.chomp
+
+puts "and what is your last name"
+lname= gets.chomp
+
+puts "Nice to meet you, #{fname} #{mname} #{lname}"
---
+++ bigger_number.rb
@@ -0,0 +1,7 @@
+puts "what is your favorite number?"
+number= gets.chomp.to_i
+
+number += 1
+puts " #{number} is a much better number, because its bigger then yours!"
+
+
